17fbcd69bbfe29481a85a38e26a13d86cc91780f
leemyunggyu <jaylee@hatiolab.com>
2019-06-14 10:37:04 +0800

    v0.0.2

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -17,11 +17,7 @@ function excelToJson(params) {
 
   const firstSheetName = workbook.SheetNames[0]
   const worksheet = workbook.Sheets[firstSheetName]
-  console.log(
-    XLSX.utils.sheet_to_json(worksheet, {
-      raw: true
-    })
-  )
+  return XLSX.utils.sheet_to_json(worksheet, { raw: true })
 }
 
 export default function bootstrap() {
